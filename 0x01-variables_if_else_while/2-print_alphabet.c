0613800dcbe5dece1bc2ab4fa94d4ed28f642bb9
Eze <ifeanyialex2@gmail.com>
2022-04-10 00:36:55 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,45 +1,18 @@
-urn (0);
-}
-
-
-TASK 2
-
-#include <stdio.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success/correct)
- */
-int main(void)
-
-{
-char alphabet;
-
-for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-{
-putchar(alphabet);
-}
-
-putchar('\n');
-
-return (0);
-}
-TASK 3
 #include <stdio.h>
 
 /**
- * main - prints the alphabet in lowercase, and then in uppercase,
+ * main - prints the alphabet in lowercase,
  * followed by a new line
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int ch;
+	char ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
+	{
 		putchar(ch);
-	for (ch = 'A'; ch <= 'Z'; ch++)
-		putchar(ch);
+	}
 	putchar('\n');
 	return (0);
 }
