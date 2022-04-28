2a8d907f68591975137427d7a844c6315ae04420
Eze <ifeanyialex2@gmail.com>
2022-04-28 10:19:31 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -30,3 +30,15 @@ int comparator(char *s, int n1, int n2)
 	}
 	return (0);
 }
+
+/**
+ * is_palindrome - detects if a string is a palindrome.
+ * @s: string.
+ * Return: 1 if s is a palindrome, 0 if not.
+ */
+int is_palindrome(char *s)
+{
+	if (*s == '\0')
+		return (1);
+	return (comparator(s, 0, _strlen_recursion(s) - 1));
+}
