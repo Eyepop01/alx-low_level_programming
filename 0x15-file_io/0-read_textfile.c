8fab2e7525da944e54660780a823bdaef03f45c1
Eze <ifeanyialex2@gmail.com>
2022-06-01 12:01:34 -0700

    correction on o read text file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,39 +1,46 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a file and prints it to the
- * POSIX standard output.
- * @filename: the name of the file.
- * @letters: the number of characters to be read from the file
- *
- * Return: the number of characters read from file.
+ * read_textfile - reads a text file and prints it to the POSIX std-output.
+ * @filename: the file to open.
+ * @letters: number of letters it should read and print.
+ * Return: number of letters it could read and print, or 0 if:
+ * - filename is NULL.
+ * - the file can not be opened or read.
+ * - write fails or does not write the expected amount of bytes.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	int r, w;
-	char *buf;
+	int file_descriptor = -1;
+	ssize_t output = 0;
+	char *buffer;
 
 	if (!filename)
 		return (0);
 
-
-	buf = malloc(sizeof(char) * letters);
-	if (!buf)
+	file_descriptor = open(filename, O_RDONLY);
+	if (file_descriptor < 0)
 		return (0);
 
-	fd = open(filename, O_RDONLY);
-	r = read(fd, buf, letters);
-	w = write(STDOUT_FILENO, buf, r);
-
-	if (fd == -1 || r == -1 || w == -1 || w != r)
+	buffer = malloc(sizeof(char) * letters);
+	if (!buffer)
 	{
-		free(buf);
+		close(file_descriptor);
 		return (0);
 	}
 
-	free(buf);
-	close(fd);
+	output = read(file_descriptor, buffer, letters);
+	if (output < 0)
+	{
+		free(buffer);
+		close(file_descriptor);
+		return (0);
+	}
 
-	return (w);
+	output = write(STDOUT_FILENO, buffer, output);
+	free(buffer);
+	close(file_descriptor);
+	if (output < 0)
+		return (0);
+	return (output);
 }
