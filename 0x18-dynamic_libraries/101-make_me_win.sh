416465be516f276674359bdb13acf417f44ce73e
Eze <ifeanyialex2@gmail.com>
2022-07-13 22:38:35 -0700

    101 fix

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-<<<<<<< HEAD
 gcc *.o -fPIC -shared -o libruin.so
 LD_PRELOAD=$PWD/libruin.so
-=======
-wget -P .. https://raw.githubusercontent.com/eyepop01/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
-export LD_PRELOAD="$PWD/../libgiga.so"
->>>>>>> 20ea75596ccf3b350498b2a389cb25dcb2832391
