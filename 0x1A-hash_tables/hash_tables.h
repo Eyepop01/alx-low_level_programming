4139de25305e16509864321d63f7aa8f332c56c1
Eze <ifeanyialex2@gmail.com>
2022-07-28 13:54:07 -0700

    hash_tables

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,25 +1,30 @@
-#ifndef HASH_TABLES_H
-#define HASH_TABLES_H
+#ifndef _HASH_TABLE_
+#define _HASH_TABLE_
 
-#include <stdlib.h>
+
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
+
 /**
- * struct hash_node_s - nodes for each key value pair within hash table
- * @key : The string key is unique in the HashTable
- * @value : The value corresponding to a key
- * @next : A pointer to the next node of the list
+ * struct hash_node_s - Node of a hash table
+ *
+ * @key: The key, string
+ * The key is unique in the HashTable
+ * @value: The value corresponding to a key
+ * @next: A pointer to the next node of the List
  */
 typedef struct hash_node_s
 {
-	char *key;
-	char *value;
-	struct hash_node_s *next;
+     char *key;
+     char *value;
+     struct hash_node_s *next;
 } hash_node_t;
 
 /**
- * struct hash_table_s - hash table data structure
+ * struct hash_table_s - Hash table data structure
+ *
  * @size: The size of the array
  * @array: An array of size @size
  * Each cell of this array is a pointer to the first node of a linked list,
@@ -27,8 +32,8 @@ typedef struct hash_node_s
  */
 typedef struct hash_table_s
 {
-	unsigned long int size;
-	hash_node_t **array;
+     unsigned long int size;
+     hash_node_t **array;
 } hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
@@ -38,4 +43,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
+
+
 #endif
